2c1eee5ba92525ab360d689c45ed69afb381d29f
Chrisqcwx <3075401625@qq.com>
2023-10-22 22:10:23 +0800

    refactor package

---
+++ src/modelinversion/utils.py
@@ -0,0 +1,16 @@
+import sys
+
+class Tee(object):
+    def __init__(self, name, mode):
+        self.file = open(name, mode)
+        self.stdout = sys.stdout
+        sys.stdout = self
+    def __del__(self):
+        sys.stdout = self.stdout
+        self.file.close()
+    def write(self, data):
+        if not '...' in data:
+            self.file.write(data)
+        self.stdout.write(data)
+    def flush(self):
+        self.file.flush()
