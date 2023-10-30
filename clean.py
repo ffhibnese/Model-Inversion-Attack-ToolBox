3e5f15f82fac14df6006a90441a30da39d716927
Chrisqcwx <92083012+Chrisqcwx@users.noreply.github.com>
2023-10-30 12:54:57 +0800

    refactor clean.py (#18)
    * refactor: di, mirror
    
    * feat: clean.py

--- clean.py
+++ clean.py
@@ -1,5 +1,9 @@
 import os
 
 if __name__ == '__main__':
-    # os.system('rm -rf ./cache/*')
-    os.system('rm -rf ./results/*')
+    os.system('rm -rf ./cache/*')
+    result_dir = './results'
+    for dir_name in os.listdir(result_dir):
+        remove_dir = os.path.join(result_dir, dir_name)
+        if os.path.isdir(remove_dir):
+            os.system(f'rm -rf {remove_dir}')
