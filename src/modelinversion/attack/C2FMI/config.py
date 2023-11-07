aa6877748d4538c9920a4800db5b1ffb3b05d72a
final-solution <2507586353@qq.com>
2023-11-08 00:31:16 +0800

    c2fmi

---
+++ src/modelinversion/attack/C2FMI/config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class C2FMIConfig:
+    
+    target_name: str
+    eval_name: str
+    gan_target_name: str
+    ckpt_dir: str
+    dataset_dir: str
+    result_dir: str
+    cache_dir: str
+    
+    dataset_name: str
+    gan_dataset_name: str
+    
+    batch_size: int = 60
+    target_labels: list = field(default_factory = lambda : list(range(300)))
+    device: str = 'cpu'
