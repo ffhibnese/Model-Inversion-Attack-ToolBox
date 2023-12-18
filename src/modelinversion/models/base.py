76a38e2c702aa729addf839974f94e75f1d2d66d
Chrisqcwx <3075401625@qq.com>
2023-12-18 13:34:12 +0800

    feat: add base method to freeze for TL

--- src/modelinversion/models/base.py
+++ src/modelinversion/models/base.py
@@ -1,5 +1,7 @@
+
+from abc import abstractmethod
+
 from torch import nn
-from abc import ABCMeta, abstractmethod
 
 class BaseTargetModel(nn.Module):
     
@@ -21,5 +23,13 @@ class BaseTargetModel(nn.Module):
 
         Returns:
             list: list of hidden features.
+        """
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def freeze_front_layers(self) -> None:
+        """
+        The forward process T(x) can be viewed as C(E(x)). x -> E -> C -> y.
+        The function of this method is to freeze E. It is used by TL defense method
         """
         raise NotImplementedError()
