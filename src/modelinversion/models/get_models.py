d285d33fbe5b82b931c975ccadb4aaf30de8db38
Chrisqcwx <3075401625@qq.com>
2023-12-30 01:15:08 +0800

    feat: add classifiers (#53)
    * feat: add efficientnet
    
    * feat: add defense function for efficientnet
    
    * add torchvision model

--- src/modelinversion/models/get_models.py
+++ src/modelinversion/models/get_models.py
@@ -6,6 +6,7 @@ from .inception.incv1 import InceptionResnetV1
 from .vit.vit import ViT
 import torchvision.models as tv_models
 from .defense_wrapper import VibWrapper
+from .efficientnet.efficientnet import *
 
 NUM_CLASSES = {
     'celeba': 1000,
@@ -52,8 +53,27 @@ def get_model(model_name: str, dataset_name: str, device='cpu', backbone_pretrai
             dropout=0.1,
             emb_dropout=0.1
         )
+    elif model_name.startswith('efficientnet'):
+        suffix = model_name[-2:]
+        if suffix == 'b0':
+            model = EfficientNet_b0(num_classes, pretrained=backbone_pretrain)
+        elif suffix == 'b1':
+            model = EfficientNet_b1(num_classes, pretrained=backbone_pretrain)
+        elif suffix == 'b2':
+            model = EfficientNet_b2(num_classes, pretrained=backbone_pretrain)
+        else:
+            raise RuntimeError(f'model {model_name} is NOT supported')
     else:
-        raise RuntimeError(f'model {model_name} is NOT supported')
+        if num_classes == 1000:
+            try:
+                model = tv_models.__dict__[model_name](pretrained=True)
+            except:
+                raise RuntimeError(f'model {model_name} is NOT supported')
+        else:
+            raise RuntimeError(f'model {model_name} is NOT supported')
+        
+        print('get model from torchvision')
+        
     
     if defense_type.lower() in ['mid', 'vib']:
         model = VibWrapper(model, model.get_feature_dim(), num_classes)
