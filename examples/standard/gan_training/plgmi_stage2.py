afcd66aab1332f7b4306c779c054eb226cee9269
Chrisqcwx <3075401625@qq.com>
2024-03-25 13:04:43 +0800

    feat: add refactor for plg

---
+++ examples/standard/gan_training/plgmi_stage2.py
@@ -0,0 +1,99 @@
+import sys
+import os
+import time
+
+sys.path.append('../../../src')
+
+import kornia
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import ToTensor
+
+from modelinversion.models import IR152_64, PlgmiGenerator64, PlgmiDiscriminator64
+from modelinversion.train.gan import PlgmiGanTrainer
+from modelinversion.utils import Logger, SimpleLatentsSampler
+from modelinversion.datasets import InfiniteSamplerWrapper
+
+if __name__ == '__main__':
+    
+    top_k = 30
+    num_classes = 1000
+    target_model_ckpt_path = '<fill it>'
+    dataset_path = '<fill it>'
+    experiment_dir = '<fill it>'
+    
+    batch_size = 64
+    max_iters = 30000
+    
+    device_ids_str = '0'
+    
+    # prepare logger
+    
+    now_time = time.strftime(r'%Y%m%d_%H%M', time.localtime(time.time()))
+    logger = Logger(experiment_dir, f'train_gan_{now_time}.log')
+    
+    # prepare devices
+    
+    os.environ["CUDA_VISIBLE_DEVICES"] = device_ids_str
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = torch.device(device)
+    gpu_devices = [i for i in range(torch.cuda.device_count())]
+    
+    # prepare target models
+    
+    target_model = IR152_64(num_classes=num_classes)
+    target_model.load_state_dict(torch.load(target_model_ckpt_path, map_location='cpu')['state_dict'])
+    target_model = nn.DataParallel(target_model, device_ids=gpu_devices).to(device)
+    
+    # prepare dataset
+    dataset = ImageFolder(dataset_path, transform=ToTensor())
+    dataloader = iter(DataLoader(dataset, batch_size=batch_size, sampler=InfiniteSamplerWrapper(dataset)))
+    
+    # prepare GANs
+    
+    z_dim = 128
+    
+    latents_sampler = SimpleLatentsSampler(z_dim)
+    
+    generator = PlgmiGenerator64(num_classes, dim_z=z_dim)
+    discriminator = PlgmiDiscriminator64(num_classes)
+    
+    generator = nn.DataParallel(generator, device_ids=gpu_devices).to(device)
+    discriminator = nn.DataParallel(discriminator, device_ids=gpu_devices).to(device)
+    
+    gen_optimizer = torch.optim.Adam(generator.parameters(), lr=0.0002, betas=(0.0, 0.9))
+    dis_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.0, 0.9))
+    
+    
+    # prepare trainer
+    
+    data_augment = kornia.augmentation.container.ImageSequential(
+        kornia.augmentation.RandomResizedCrop((64, 64), scale=(0.8, 1.0), ratio=(1.0, 1.0)),
+        kornia.augmentation.ColorJitter(brightness=0.2, contrast=0.2, p=0.5),
+        kornia.augmentation.RandomHorizontalFlip(),
+        kornia.augmentation.RandomRotation(5),
+    )
+    
+    trainer = PlgmiGanTrainer(
+        experiment_dir=experiment_dir,
+        batch_size=batch_size,
+        latents_sampler=latents_sampler,
+        generator=generator,
+        discriminator=discriminator,
+        num_classes=num_classes,
+        target_model=target_model,
+        classification_loss_fn='max_margin',
+        device=device,
+        augment=data_augment,
+        gen_optimizer=gen_optimizer,
+        dis_optimizer=dis_optimizer,
+        save_ckpt_iters=1000,
+        show_images_iters=1000,
+        show_train_info_iters=100
+    )
+    
+    # train gan
+    
+    trainer.train(dataloader, max_iters)
