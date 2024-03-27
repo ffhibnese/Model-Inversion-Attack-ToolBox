68b1cc1b39e3979cad66d50b2956951456736730
Chrisqcwx <3075401625@qq.com>
2024-03-27 11:09:45 +0800

    fix: example error

--- examples/standard/classifier_training/default.py
+++ examples/standard/classifier_training/default.py
@@ -9,7 +9,7 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
-from torchvision.transforms import ToTensor, Compose, ColorJitter, RandomResizedCrop, RandomHorizontalFlip
+from torchvision.transforms import ToTensor, Compose, ColorJitter, RandomResizedCrop, RandomHorizontalFlip, Normalize
 
 from modelinversion.models import TorchvisionClassifierModel
 from modelinversion.train import SimpleTrainer, SimpleTrainConfig
@@ -45,10 +45,10 @@ if __name__ == '__main__':
     
     # prepare target model
     
-    target_model = TorchvisionClassifierModel(arch_name=torchvison_model_name, num_classes=num_classes)
-    target_model = nn.DataParallel(target_model, device_ids=gpu_devices).to(device)
+    model = TorchvisionClassifierModel(arch_name=torchvison_model_name, num_classes=num_classes)
+    model = nn.DataParallel(model, device_ids=gpu_devices).to(device)
     
-    optimizer = torch.optim.Adam(target_model.parameters(), lr=0.001, betas=[0.9, 0.999])
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=[0.9, 0.999])
     lr_schedular = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[75, 90], gamma=0.1)
     
     # prepare dataset
@@ -57,9 +57,13 @@ if __name__ == '__main__':
         ToTensor(),
         RandomResizedCrop(size=(224, 224), scale=(0.85, 1), ratio=(1, 1), antialias=True),
         ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1, hue=0.1),
-        RandomHorizontalFlip(p=0.2)
+        RandomHorizontalFlip(p=0.2),
+        Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5])
     ]))
-    test_dataset = ImageFolder(test_dataset_path, ToTensor())
+    test_dataset = ImageFolder(test_dataset_path, Compose([
+        ToTensor(),
+        Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5])
+    ]))
     
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=pin_memory)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, pin_memory=pin_memory)
@@ -70,6 +74,7 @@ if __name__ == '__main__':
         experiment_dir=experiment_dir,
         save_name=save_name,
         device=device,
+        model=model,
         optimizer=optimizer,
         lr_scheduler=lr_schedular,
         loss_fn='cross_entropy'
